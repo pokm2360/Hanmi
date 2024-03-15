e595a2945dad1b0de2d331f81ca9653c53b823f8
pokm <por0632@naver.com>
2024-03-15 12:20:01 +0900

    3/15 진행중

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-// circle
+
+new Swiper('.main2 .swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: {
+    delay: 4000
+  },
+  slidesPerView: 1,
+
+  pagination: {
+    el: '.main2 .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.main2 .swiper-button-next',
+    prevEl: '.main2 .swiper-button-prev',
+  },
+
+});
