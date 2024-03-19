939d4d428584d81bf4f62c5e1bb7b2edc3633011
pokm <por0632@naver.com>
2024-03-19 14:51:56 +0900

    3/19 중간저장

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ new Swiper('.main2 .swiper', {
   direction: 'horizontal',
   loop: true,
   autoplay: {
-    delay: 4000
+    delay: 2000 ,
+    disableOnInteraction: false
   },
   slidesPerView: 1,
 
@@ -15,9 +16,47 @@ new Swiper('.main2 .swiper', {
     nextEl: '.main2 .swiper-button-next',
     prevEl: '.main2 .swiper-button-prev',
   },
-
 });
 
+var sw = 0;
+$('.btn_pause').click(function(){
+    if(sw==0){
+        $('.btn_pause').addClass('on');
+        mySwiper.autoplay.stop();
+        sw = 1;
+    }else{
+        $('.btn_pause').removeClass('on');
+        mySwiper.autoplay.start();
+        sw = 0;
+    }
+});
+
+// CDMO SWIPER
+new Swiper('.cdmo .swiper' , {
+  direction: 'horizontal',
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination-progressbar-fill',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.main2 .swiper-button-next',
+    prevEl: '.main2 .swiper-button-prev',
+  },
+});
+
+
+// new Swiper('.medicine', {
+//   direction: 'horizontal',
+//   loop: true,
+//   slidesPerView: 'auto',
+//   spaceBetween: 30,
+//   breakpoints: {
+//     1024: {
+//     spaceBetween: 16
+//   }
+// }
+// })
 
 // to-Top scroll
 const toTopEl = document.querySelector('#to-Top');
@@ -41,3 +80,7 @@ toTopEl.addEventListener('click', function () {
 
 
 // 마우스 커서 효과는 구글링해서 아무거나
+
+// 중앙배치
+// 슬라이드 제작
+// 헤더 전환버튼 제작
